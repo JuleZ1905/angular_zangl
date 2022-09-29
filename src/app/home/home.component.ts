a2c6cc95d4fed93aeeaf0cc9562154817bfe1ff8
JuleZ_2k4 <8096@htl.rennweg.at>
2022-09-29 16:45:58 +0200

    more css added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,21 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <mat-toolbar color="primary">
-      <a routerLink="/home"><span class="home-link">Home</span></a>
-    </mat-toolbar>
-    <h1 class="header">Your Tasks</h1>
-    <div>
-      <div class="tasks">
-        <app-task
-          class="task-item"
-          *ngFor="let task of tasks"
-          [task]="task"
-        ></app-task>
-        <app-task [isAddTask]="true"></app-task>
+    <div class="home">
+      <div class="heading-view">
+        <mat-toolbar color="primary">
+          <a routerLink="/home"><span class="home-link">Home</span></a>
+        </mat-toolbar>
+        <h1 class="header">Your Tasks</h1>
       </div>
-      <div class="edit">
-        
+      <div class="task-view">
+        <div class="tasks">
+          <app-task
+            class="task-item"
+            *ngFor="let task of tasks"
+            [task]="task"
+          ></app-task>
+          <app-task [isAddTask]="true"></app-task>
+        </div>
+        <div class="v-line"></div>
+        <app-edit-task> </app-edit-task>
       </div>
     </div>
   `,
@@ -29,7 +32,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     for (let index = 2; index < 50; index++) {
       this.tasks.push({
-        name: 'task ' + index,
+        name: 'taskdefuehfuehfuhefuheufuehfuehfuheufhe' + index,
         description: 'description ' + index,
       });
     }
